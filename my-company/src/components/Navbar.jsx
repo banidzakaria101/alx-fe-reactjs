2e35968b7a66154cb638896e50777a19ad4c7f5e
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-18 13:25:08 +0100

    update style in navebar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
-        <nav style={{ backgroundColor: '#222', padding: '10px', textAlign: 'center' }}>
-            <Link to="/" style={{ color: 'white', marging:'0 15px' }}>Home</Link>
-            <Link to="/about" style={{ color: 'white', marging:'0 15px' }}>About</Link>
-            <Link to="/services" style={{ color: 'white', marging:'0 15px' }}>Services</Link>
-            <Link to="/contact" style={{ color: 'white', marging:'0 15px' }}>Contact</Link>
-
-
-
-        </nav>
-    );
+  return (
+    <nav style={{
+      backgroundColor: '#222',
+      padding: '10px',
+      display: 'flex',
+      justifyContent: 'center',
+      gap: '15px',
+    }}>
+      <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: 'white', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
 }
 
 export default Navbar;
