2c8722e5172a88fbe146f97296fa0c30f9bf1715
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-23 14:06:02 +0100

    update 7

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,4 @@
-import useRecipeStore from '../recipeStore';
-
+import useRecipeStore from "./RecipeStore";
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
