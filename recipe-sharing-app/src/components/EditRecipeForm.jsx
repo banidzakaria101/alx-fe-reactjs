c43052e2d074e2975c53fcc29a6d5ba6baaa38da
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-23 17:33:54 +0100

    update handleSubmit

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,28 +1,30 @@
-import { useRecipeStore } from './recipeStore';
-import { useState }from 'react';
-
+import { useRecipeStore } from "./recipeStore";
+import { useState } from "react";
 
 const EditRecipeForm = ({ recipe, onFinish }) => {
-    const [title, setTitle] =useState(recipe.title);
-    const[description, setDescription] = useState(recipe.description);
-    const updateRecipe = useRecipeStore((state) => state.updateRecipe);
+  const [title, setTitle] = useState(recipe.title);
+  const [description, setDescription] = useState(recipe.description);
+  const updateRecipe = useRecipeStore((state) => state.updateRecipe);
 
-    const handllesubmit = (e) => {
-        e.preventDefault();
-        updateRecipe({id: recipe.id, title, description});
-        onFinish();
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateRecipe({ id: recipe.id, title, description });
+    onFinish();
+  };
 
-    return (
-        <form onSubmit={handllesubmit}>
-            <input value={title}
-            onChange={(e) => setTitle(e.target.value)} />
-            <textarea value={description}
-            onChange={(e) => setDescription(e.target.value)} />
-            <button type='submit'>save</button>
-            <button type='button' onClick={onFinish}>Cancel</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit">save</button>
+      <button type="button" onClick={onFinish}>
+        Cancel
+      </button>
+    </form>
+  );
 };
 
 export default EditRecipeForm;
