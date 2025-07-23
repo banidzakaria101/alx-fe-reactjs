e86e417ef825e53a230626b2a74230362c04715e
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-23 16:09:50 +0100

    create RecipeDetails component

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,30 @@
+
+
+const RecipeDetails = () => {
+    const {id} = useParams();
+    const recipeId = Number(id);
+    const recipe = useRecipeStore((state) => state.recipes.find((recipe) => recipe.id ===recipeId));
+    const [isEditing, setIsEditing] = useState(false);
+    const navigate = useNavigate();
+
+    if (!recipe)  return <p>Recipe non found</p>;
+
+    return (
+        <div>
+            {isEditing ? (
+                <EditRecipeForm recipe={recipe} onFinish={() => setIsEditing(false)} />
+            ) : (
+                <>
+                <h1>{recipe.title}</h1>
+                <p>{recipe.description}</p>
+                <button onClick={() => setIsEditing(true)}>Edit</button>
+                <DleteRecipeButton 
+                recipeId={recipeId}
+                onDeleted={ () => navigate('/')}/>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default RecipeDetails;
