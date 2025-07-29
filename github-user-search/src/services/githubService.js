74241eb9d1ced29f0fde8aa731c95702b1c9b937
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-29 12:59:21 +0100

    update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,25 +1,20 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://api.github.com' });
-
 export async function searchUsers({ username, location, minRepos }, page = 1) {
   let query = `${username} in:login type:user`;
   if (location) query += ` location:${location}`;
   if (minRepos) query += ` repos:>=${minRepos}`;
 
-  const res = await api.get('/search/users', {
-    params: {
-      q: query,
-      per_page: 10,
-      page,
-    },
-  });
+
+  const url = `https://api.github.com/search/users?q=${encodeURIComponent(query)}&per_page=10&page=${page}`;
+
+  const res = await axios.get(url);
 
   const users = res.data.items;
 
   const detailedUsers = await Promise.all(
     users.map(u =>
-      api.get(`/users/${u.login}`).then(r => r.data).catch(() => null)
+      axios.get(`https://api.github.com/users/${u.login}`).then(r => r.data).catch(() => null)
     )
   );
 
