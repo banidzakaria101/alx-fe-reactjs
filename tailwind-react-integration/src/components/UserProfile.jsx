6152ea2b6fa00e77db343166daa570d51521639f
zakariabanid101 <banidzakaria2@gmail.com>
2025-08-08 10:33:09 +0100

    add styling to paragraph

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
     <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg">
       <img src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="rounded-full w-36 h-36 mx-auto">Developer at Exemple Co. Love to write code and explore nex technologies</p>
+      <p className="rounded-full w-36 h-36 mx-auto text-gray-600 text-base">Developer at Exemple Co. Love to write code and explore nex technologies</p>
     </div>
   )
 }
