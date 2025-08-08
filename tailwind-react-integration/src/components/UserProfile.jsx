384846c3a9f34c74155327832cbb82f0bab921a0
zakariabanid101 <banidzakaria2@gmail.com>
2025-08-08 10:43:48 +0100

    correct h1 styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile() {
   return (
     <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
       <img src="https://via.placeholder.com/150 sm:w-24 sm:h-24 md:w-36 md:h36" alt="User" />
-      <h1 className="text-xl text-blue-800 my-4 sm:tex-lg md:xl">John Doe</h1>
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
       <p className="rounded-full w-36 h-36 mx-auto text-gray-600 text-base sm:text-sm md:tezt-base">Developer at Exemple Co. Love to write code and explore nex technologies</p>
     </div>
   )
